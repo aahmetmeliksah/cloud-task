c728992bd1fa9f57aac8af15216c04189fa3f07c
unknown <aahmetmeliksah@gmail.com>
2022-06-16 17:55:28 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+function App() {
+  return (
+    <Container>
+       <Typography variant='h5'>This is APP Component</Typography>
+    </Container>
+  );
+}
+
+export default App;
