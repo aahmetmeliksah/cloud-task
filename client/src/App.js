4d3bae82e08dc3b529dd65c06bf0bfd3cfc19fa9
unknown <aahmetmeliksah@gmail.com>
2022-06-18 20:41:07 +0300

    hero done

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { Container } from '@material-ui/core';
 
 import './style.css'
 // components
 import Header from './components/common/header/Header'
 import HeroComponent from './components/hero/HeroComponent';
+import About from './components/about/About';
+
+import img1 from "./images/female-doctor-with-presenting-hand-gesture.jpg"
+import img2 from "./images/medium-shot-man-getting-vaccine.jpg"
 
 function App() {
   return (
-    <>
+    <Container>
       <Header />
       <HeroComponent />
-    </>
+      <About />
+
+      <div className="images-app">
+        <img src={img1} />
+        <img style={{marginLeft: "2rem"}} src={img2} />
+      </div>
+    </Container>
   );
 }
 
---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import "./aboutStyles.css"
+
+function About() {
+    return (
+        <section className='about-container'>
+          
+          <div className="about-dr">
+            <h2 style={{color: "black"}}>Meet Dr. Carson</h2>
+            <p>
+            Protect yourself and others by wearing masks and washing hands frequently. 
+            Outdoor is safer than indoor for gatherings or holding events. People who get sick with Coronavirus disease (COVID-19) 
+            will experience mild to moderate symptoms and recover without special treatments. 
+            <br /> <br />
+            You can feel free to use this CSS template for your medical profession or health care related websites. 
+            You can support us a little via PayPal if this template is good and useful for your work.
+            </p>
+          </div>
+            
+          
+          <div className="experience">
+            <p><span id='twelve'>12</span> Years <br /> <span style={{marginLeft: "1.5rem"}}>of Experiences</span>  </p>
+          </div>
+        </section>
+    )
+}
+
+export default About
